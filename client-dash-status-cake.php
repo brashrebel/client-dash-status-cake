7b2992a84be8c2e97caaa46c24a8fc3c979b11ea
Kyle <kyle@realbigmarketing.com>
2014-06-21 12:03:46 -0400

    Specifying test
    Added an option for specific test and output a bunch more pieces of
    information.

--- client-dash-status-cake.php
+++ client-dash-status-cake.php
@@ -9,9 +9,6 @@ Author URI: http://realbigmarketing.com/staff/kyle
 
 class CDStatusCake {
 
-	/*
-	* These variables you can change
-	*/
 	// Define the plugin name
 	private $plugin = 'Client Dash Status Cake Add-on';
 	// Setup your prefix
@@ -22,16 +19,11 @@ class CDStatusCake {
 	private $tab = 'uptime';
 	// Set this to the page you want your tab to appear on (account, help and reports exist in Client Dash)
 	private $page = 'reports';
-	// A text field option
-	private $username = '_username';
-	// A URL/text field option
-	private $api = '_api';
 
-	/*
-	* This constructor function sets up what happens when the plugin
-	* is activated. It is where you'll place all your actions, filters
-	* and other setup components.
-	*/
+	private $username = '_username';
+	private $api = '_api';
+	private $test = '_test';
+
 	public function __construct() {
 		add_action( 'admin_notices', array( $this, 'notices' ) );
 		add_filter( 'cd_tabs', array( $this, 'add_tab' ) );
@@ -55,12 +47,14 @@ class CDStatusCake {
 	public function register_settings() {
 		register_setting( 'cd_options_general', $this->pre.$this->username, 'esc_html' );
 		register_setting( 'cd_options_general', $this->pre.$this->api, 'esc_html' );
+		register_setting( 'cd_options_general', $this->pre.$this->test, 'esc_html' );
 	}
 
 	// Add settings to General tab
 	public function settings_display() {
 		$username = $this->pre.$this->username;
 		$api = $this->pre.$this->api;
+		$test = $this->pre.$this->test;
 		?>
 	<table class="form-table">
 		<tbody>
@@ -87,6 +81,16 @@ class CDStatusCake {
 					value="<?php echo get_option( $api ); ?>" />
 				</td>
 			</tr>
+			<tr valign="top">
+				<th scope="row">
+					<label for="<?php echo $test; ?>">Test ID</label>
+				</th>
+				<td><input type="text" 
+					id="<?php echo $test; ?>" 
+					name="<?php echo $test; ?>" 
+					value="<?php echo get_option( $test ); ?>" />
+				</td>
+			</tr>
 		</tbody>
 	</table>
 	<?php }
@@ -101,39 +105,69 @@ class CDStatusCake {
 	public function tab_contents() {
 		$un = get_option( $this->pre.$this->username );
 		$api = get_option( $this->pre.$this->api );
-		$content = wp_remote_retrieve_body( wp_remote_get( 'https://statuscake.com/API/Tests/?API='. $api .'&Username='. $un ) );
+		$test = get_option( $this->pre.$this->test );
+		$content = wp_remote_retrieve_body( wp_remote_get( 'https://statuscake.com/API/Tests/Details/?TestID='. $test .'&API='. $api .'&Username='. $un ) );
 
 		$content = json_decode( $content );
+		// Uncomment the next line to see the raw data
 		//print_r($content);
 		?>
 		<table class="form-table">
 			<tr valign="top">
 				<th scope="row">Test ID</th>
-				<td><?php echo $content[0]->TestID; ?></td>
+				<td><?php echo $content->TestID; ?></td>
 			</tr>
 			<tr valign="top">
-				<th scope="row">Test Type</th>
-				<td><?php echo $content[0]->TestType; ?></td>
+				<th scope="row">Test type</th>
+				<td><?php echo $content->TestType; ?></td>
 			</tr>
 			<tr valign="top">
-				<th scope="row">Website Name</th>
-				<td><?php echo $content[0]->WebsiteName; ?></td>
+				<th scope="row">Website name</th>
+				<td><?php echo $content->WebsiteName; ?></td>
 			</tr>
 			<tr valign="top">
-				<th scope="row">Contact Group</th>
-				<td><?php echo $content[0]->ContactGroup; ?></td>
+				<th scope="row">Contact group</th>
+				<td><?php echo $content->ContactGroup; ?></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">Contact ID</th>
+				<td><?php echo $content->ContactID; ?></td>
 			</tr>
 			<tr valign="top">
 				<th scope="row">Status</th>
-				<td><?php echo $content[0]->Status; ?></td>
+				<td><?php echo $content->Status; ?></td>
 			</tr>
 			<tr valign="top">
 				<th scope="row">Uptime</th>
-				<td><?php echo $content[0]->Uptime; ?></td>
+				<td><?php echo $content->Uptime; ?></td>
 			</tr>
 			<tr valign="top">
-				<th scope="row">Performance</th>
-				<td><?php echo $content[0]->Performance; ?></td>
+				<th scope="row">Check rate</th>
+				<td><?php echo $content->CheckRate; ?></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">Timeout</th>
+				<td><?php echo $content->Timeout; ?></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">Logo</th>
+				<td><?php echo $content->LogoImage; ?></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">Website Host</th>
+				<td><?php echo $content->WebsiteHost; ?></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">Last tested</th>
+				<td><?php echo $content->LastTested; ?></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">Next test location</th>
+				<td><?php echo $content->NextLocation; ?></td>
+			</tr>
+			<tr valign="top">
+				<th scope="row">Down times</th>
+				<td><?php echo $content->DownTimes; ?></td>
 			</tr>
 		</table>
 		<?php
