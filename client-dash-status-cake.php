18c3624e8a99b554d7b5c9b2a80a0ed69f9f2552
Joel <joel.worsham@gmail.com>
2014-08-12 17:01:00 -0400

    Brought up to speed with CD 1.5

--- client-dash-status-cake.php
+++ client-dash-status-cake.php
@@ -2,148 +2,188 @@
 /*
 Plugin Name: Client Dash Status Cake Add-on
 Description: Integrates Status Cake with Client Dash
-Version: 0.2.1
+Version: 0.2.2
 Author: Kyle Maurer
 Author URI: http://realbigmarketing.com/staff/kyle
 */
 
-class CDStatusCake {
+function cd_status_cake() {
 
-	// Define the plugin name
-	private $plugin = 'Client Dash Status Cake Add-on';
-	// Setup your prefix
-	private $pre = 'cdsc';
-	// Set this to be your tab name
-	private $block_name = 'Uptime';
-	// Set the tab slug
-	private $tab = 'uptime';
-	// Set this to the page you want your tab to appear on (account, help and reports exist in Client Dash)
-	private $page = 'reports';
+	if ( ! class_exists( 'ClientDash' ) ) {
+		add_action( 'admin_notices', 'cdsc_notice' );
 
-	private $username = '_username';
-	private $api = '_api';
-	private $test = '_test';
-
-	public function __construct() {
-		add_action( 'admin_notices', array( $this, 'notices' ) );
-		add_action( 'plugins_loaded', array( $this, 'content_block' ) );
-		add_action( 'admin_init', array( $this, 'register_settings' ) );
-		add_action( 'cd_settings_general_tab', array( $this, 'settings_display' ), 11 );
-		add_action( 'admin_enqueue_scripts', array( $this, 'register_styles'), 11 );
+		return;
 	}
 
-	public function content_block() {
-		cd_content_block( $this->block_name, $this->page, $this->tab, array( $this, 'block_contents' ) );
-	}
+	class CDStatusCake extends ClientDash {
 
-	public function register_styles() {
-		wp_register_style( $this->pre, plugin_dir_url(__FILE__).'style.css' );
-		$page = get_current_screen();
-		$tab = $_GET['tab'];
+		// Define the plugin name
+		private $plugin = 'Client Dash Status Cake Add-on';
+		// Setup your prefix
+		private $pre = 'cdsc';
+		// Set this to be your tab name
+		private $block_name = 'Uptime';
+		// Set the tab slug
+		private $tab = 'Uptime';
+		// Set the settings tab
+		public $settings_tab = 'Status Cake';
+		// Set this to the page you want your tab to appear on (account, help and reports exist in Client Dash)
+		private $page = 'Reports';
 
-		if ( $page->id != 'dashboard_page_cd_'.$this->page && $tab != $this->tab )
-			return;
+		private $username = '_username';
+		private $api = '_api';
+		private $test = '_test';
 
-		wp_enqueue_style( $this->pre );
-	}
+		public function __construct() {
 
-	// Notices for if CD is not active (no need to change)
-	public function notices() {
-		if ( !is_plugin_active( 'client-dash/client-dash.php' ) ) { ?>
-		<div class="error">
-			<p><?php echo $this->plugin; ?> requires <a href="http://w.org/plugins/client-dash">Client Dash</a> version 1.4 or greater.
-			Please install and activate <b>Client Dash</b> to continue using.</p>
-		</div>
+			add_action( 'admin_init', array( $this, 'register_settings' ) );
+			add_action( 'admin_enqueue_scripts', array( $this, 'register_styles' ), 11 );
+
+			$this->add_content_section(
+				array(
+					'name'     => $this->block_name,
+					'page'     => $this->page,
+					'tab'      => $this->tab,
+					'callback' => array( $this, 'block_contents' )
+				)
+			);
+
+			$this->add_content_section(
+				array(
+					'name'     => 'Status Cake',
+					'page'     => 'Settings',
+					'tab'      => $this->settings_tab,
+					'callback' => array( $this, 'settings_display' )
+				)
+			);
+		}
+
+		public function register_styles() {
+
+			wp_register_style( $this->pre, plugin_dir_url( __FILE__ ) . 'style.css' );
+
+			$current_page = isset( $_GET['page'] ) ? $_GET['page'] : null;
+			$current_tab  = isset( $_GET['tab'] ) ? $_GET['tab'] : null;
+
+			$page_ID         = $this->translate_name_to_id( $this->page );
+			$tab_ID          = $this->translate_name_to_id( $this->tab );
+			$settings_tab_ID = $this->translate_name_to_id( $this->settings_tab );
+
+			// Only add style if on extension tab or on extension settings tab
+			if ( ( $current_page == $page_ID && $current_tab == $tab_ID )
+			     || ( $current_page == 'cd_settings' && $current_tab == $settings_tab_ID )
+			) {
+				wp_enqueue_style( $this->pre );
+			}
+		}
+
+		// Register settings
+		public function register_settings() {
+
+			register_setting(
+				'cd_options_' . $this->translate_name_to_id( $this->settings_tab ),
+				$this->pre . $this->username,
+				'esc_html'
+			);
+
+			register_setting(
+				'cd_options_' . $this->translate_name_to_id( $this->settings_tab ),
+				$this->pre . $this->api,
+				'esc_html'
+			);
+
+			register_setting(
+				'cd_options_' . $this->translate_name_to_id( $this->settings_tab ),
+				$this->pre . $this->test,
+				'esc_html'
+			);
+		}
+
+		// Add settings to General tab
+		public function settings_display() {
+
+			$username = $this->pre . $this->username;
+			$api      = $this->pre . $this->api;
+			$test     = $this->pre . $this->test;
+			?>
+			<table class="form-table">
+				<tbody>
+				<tr valign="top">
+					<th scope="row"><h3><?php echo $this->plugin; ?> settings</th>
+				</tr>
+				<tr valign="top">
+					<th scope="row">
+						<label for="<?php echo $username; ?>">Username</label>
+					</th>
+					<td><input type="text"
+					           id="<?php echo $username; ?>"
+					           name="<?php echo $username; ?>"
+					           value="<?php echo get_option( $username ); ?>"/>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">
+						<label for="<?php echo $api; ?>">API key</label>
+					</th>
+					<td><input type="text"
+					           id="<?php echo $api; ?>"
+					           name="<?php echo $api; ?>"
+					           value="<?php echo get_option( $api ); ?>"/>
+					</td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">
+						<label for="<?php echo $test; ?>">Test ID</label>
+					</th>
+					<td><input type="text"
+					           id="<?php echo $test; ?>"
+					           name="<?php echo $test; ?>"
+					           value="<?php echo get_option( $test ); ?>"/>
+					</td>
+				</tr>
+				</tbody>
+			</table>
 		<?php
 		}
-	}
 
-	// Register settings
-	public function register_settings() {
-		register_setting( 'cd_options_general', $this->pre.$this->username, 'esc_html' );
-		register_setting( 'cd_options_general', $this->pre.$this->api, 'esc_html' );
-		register_setting( 'cd_options_general', $this->pre.$this->test, 'esc_html' );
-	}
+		// Insert the Status Cake report data
+		public function block_contents() {
 
-	// Add settings to General tab
-	public function settings_display() {
-		$username = $this->pre.$this->username;
-		$api = $this->pre.$this->api;
-		$test = $this->pre.$this->test;
-		?>
-	<table class="form-table">
-		<tbody>
-			<tr valign="top">
-				<th scope="row"><h3><?php echo $this->plugin; ?> settings</th>
-			</tr>
-			<tr valign="top">
-				<th scope="row">
-					<label for="<?php echo $username; ?>">Username</label>
-				</th>
-				<td><input type="text" 
-					id="<?php echo $username; ?>" 
-					name="<?php echo $username; ?>" 
-					value="<?php echo get_option( $username ); ?>" />
-				</td>
-			</tr>
-			<tr valign="top">
-				<th scope="row">
-					<label for="<?php echo $api; ?>">API key</label>
-				</th>
-				<td><input type="text" 
-					id="<?php echo $api; ?>" 
-					name="<?php echo $api; ?>" 
-					value="<?php echo get_option( $api ); ?>" />
-				</td>
-			</tr>
-			<tr valign="top">
-				<th scope="row">
-					<label for="<?php echo $test; ?>">Test ID</label>
-				</th>
-				<td><input type="text" 
-					id="<?php echo $test; ?>" 
-					name="<?php echo $test; ?>" 
-					value="<?php echo get_option( $test ); ?>" />
-				</td>
-			</tr>
-		</tbody>
-	</table>
-	<?php }
+			$un       = get_option( $this->pre . $this->username );
+			$api      = get_option( $this->pre . $this->api );
+			$test     = get_option( $this->pre . $this->test );
+			$response = wp_remote_get( 'https://statuscake.com/API/Tests/Details/?TestID=' . $test . '&API=' . $api . '&Username=' . $un );
+			$content  = wp_remote_retrieve_body( $response );
 
-	// Insert the Status Cake report data
-	public function block_contents() {
-		$un = get_option( $this->pre.$this->username );
-		$api = get_option( $this->pre.$this->api );
-		$test = get_option( $this->pre.$this->test );
-		$content = wp_remote_retrieve_body( wp_remote_get( 'https://statuscake.com/API/Tests/Details/?TestID='. $test .'&API='. $api .'&Username='. $un ) );
+			$content = json_decode( $content );
+			// Uncomment the next line to see the raw data
+			//print_r($content);
+			if ( empty( $un ) ) {
+				$this->error_nag( 'Please enter a valid Status Cake username in <a href="' . $this->get_settings_url( 'status_cake' ) . '">Settings</a>', 'manage_options' );
+			} elseif ( empty( $api ) ) {
+				$this->error_nag( 'Please enter a valid Status Cake API key in <a href="' . $this->get_settings_url( 'status_cake' ) . '">Settings</a>', 'manage_options' );
+			} elseif ( empty( $test ) ) {
+				$this->error_nag( 'Please enter a valid Status Cake test ID in <a href="' . $this->get_settings_url( 'status_cake' ) . '">Settings</a>', 'manage_options' );
+			} elseif ( is_wp_error( $content ) OR empty( $content ) OR ! empty( $content->Error ) ) {
+				$this->error_nag( 'Please enter valid Status Cake test values in <a href="' . $this->get_settings_url( 'status_cake' ) . '">Settings</a>', 'manage_options' );
+			} else {
 
-		$content = json_decode( $content );
-		// Uncomment the next line to see the raw data
-		//print_r($content);
-		if ( empty( $un) ) {
-			echo '<h2>Please enter a valid Status Cake username in <a href="'.cd_get_settings_url().'">Settings</a></h2>';
-		} elseif ( empty( $api) ) {
-			echo '<h2>Please enter a valid Status Cake API key in <a href="'.cd_get_settings_url().'">Settings</a></h2>';
-		} elseif ( empty( $test ) ) {
-			echo '<h2>Please enter a valid Status Cake test ID in <a href="'.cd_get_settings_url().'">Settings</a></h2>';
-		} elseif ( is_wp_error( $content ) OR empty( $content ) OR !empty($content->Error) ) {
-		echo '<h2>Please enter valid Status Cake test values in <a href="'.cd_get_settings_url().'">Settings</a></h2>';
-		} else {
+				$status = $content->Status;
+				$uptime = $content->Uptime;
+				?>
+				<div class="cdsc">
+					<h1><?php echo $content->WebsiteName; ?></h1>
 
-			$status = $content->Status;
-			$uptime = $content->Uptime;
-		?>
-		<div class="cdsc">
-			<h1><?php echo $content->WebsiteName; ?></h1>
-			<div class="cd-col-two cdsc-status">
-				<h2>Status</h2>
-				<div class="
+					<div class="cd-col-two cdsc-status">
+						<h2>Status</h2>
+
+						<div class="
 				<?php
-					if ( $status == 'Up' ) {
-						echo 'cdsc-ninety';
-					} else {
-						echo 'cdsc-fifty';
-					} ?>
+						if ( $status == 'Up' ) {
+							echo 'cdsc-ninety';
+						} else {
+							echo 'cdsc-fifty';
+						} ?>
 					">
 					<span class="dashicons 
 					<?php
@@ -153,24 +193,25 @@ class CDStatusCake {
 					">
 						<?php echo $status; ?>
 					</span>
-				</div>
-			</div>
-			<div class="cd-col-two cdsc-uptime">
-				<h2>Uptime</h2>
-				<div class="
+						</div>
+					</div>
+					<div class="cd-col-two cdsc-uptime">
+						<h2>Uptime</h2>
+
+						<div class="
 				<?php
-					if ( $uptime >= 90 ) {
-						echo 'cdsc-ninety';
-					} elseif ( $uptime >= 80 && $uptime <= 90 ) {
-						echo 'cdsc-eighty';
-					} elseif ( $uptime >= 70 && $uptime <= 80 ) {
-						echo 'cdsc-seventy';
-					} elseif ( $uptime >= 60 && $uptime <= 70 ) {
-						echo 'cdsc-sixty';
-					} elseif ( $uptime <= 60 ) {
-						echo 'cdsc-fifty';
-					}
-					?>
+						if ( $uptime >= 90 ) {
+							echo 'cdsc-ninety';
+						} elseif ( $uptime >= 80 && $uptime <= 90 ) {
+							echo 'cdsc-eighty';
+						} elseif ( $uptime >= 70 && $uptime <= 80 ) {
+							echo 'cdsc-seventy';
+						} elseif ( $uptime >= 60 && $uptime <= 70 ) {
+							echo 'cdsc-sixty';
+						} elseif ( $uptime <= 60 ) {
+							echo 'cdsc-fifty';
+						}
+						?>
 				">
 					<span class="dashicons 
 					<?php
@@ -180,34 +221,51 @@ class CDStatusCake {
 					">
 						<?php echo $uptime; ?>%
 					</span>
+						</div>
+					</div>
+					<div class="cd-col-two">
+						<h3>Test info</h3>
+						<ul>
+							<li><strong>Test ID:</strong> <?php echo $content->TestID; ?></li>
+							<li><strong>Contact ID:</strong> <?php echo $content->ContactID; ?></li>
+							<li><strong>Contact group:</strong> <?php echo $content->ContactGroup; ?></li>
+							<li><strong>Test type:</strong> <?php echo $content->TestType; ?></li>
+							<li><strong>Website host:</strong> <?php echo $content->WebsiteHost; ?></li>
+						</ul>
+					</div>
+					<div class="cd-col-two">
+						<h3>Stats</h3>
+						<ul>
+							<li><strong>Down times:</strong> <?php echo $content->DownTimes; ?></li>
+							<li><strong>Check rate:</strong> <?php echo $content->CheckRate; ?></li>
+							<li><strong>Timeout:</strong> <?php echo $content->Timeout; ?></li>
+							<li><strong>Last tested:</strong> <?php echo $content->LastTested; ?></li>
+							<li><strong>Next test location:</strong> <?php echo $content->NextLocation; ?></li>
+						</ul>
+					</div>
 				</div>
-			</div>
-			<div class="cd-col-two">
-				<h3>Test info</h3>
-				<ul>
-					<li><strong>Test ID:</strong> <?php echo $content->TestID; ?></li>
-					<li><strong>Contact ID:</strong> <?php echo $content->ContactID; ?></li>
-					<li><strong>Contact group:</strong> <?php echo $content->ContactGroup; ?></li>
-					<li><strong>Test type:</strong> <?php echo $content->TestType; ?></li>
-					<li><strong>Website host:</strong> <?php echo $content->WebsiteHost; ?></li>
-				</ul>
-			</div>
-			<div class="cd-col-two">
-				<h3>Stats</h3>
-				<ul>
-					<li><strong>Down times:</strong> <?php echo $content->DownTimes; ?></li>
-					<li><strong>Check rate:</strong> <?php echo $content->CheckRate; ?></li>
-					<li><strong>Timeout:</strong> <?php echo $content->Timeout; ?></li>
-					<li><strong>Last tested:</strong> <?php echo $content->LastTested; ?></li>
-					<li><strong>Next test location:</strong> <?php echo $content->NextLocation; ?></li>
-				</ul>
-			</div>
-		</div>
-		
-		<?php
+
+			<?php
+			}
 		}
 	}
+
+	// Instantiate the class
+	new CDStatusCake;
 }
 
-// Instantiate the class
-$cdsc = new CDStatusCake;
+add_action( 'plugins_loaded', 'cd_status_cake' );
+
+/**
+ * Notices for if CD is not active (no need to change)
+ */
+function cdsc_notice() {
+
+	?>
+	<div class="error">
+		<p>You have activated a plugin that requires <a href="http://w.org/plugins/client-dash">Client Dash</a>
+			version 1.5 or greater.
+			Please install and activate <b>Client Dash</b> to continue using.</p>
+	</div>
+<?php
+}
